8859b29316f6d8b0db12cfdddf4def9994b90d28
shaunpersad <shaunpersad@gmail.com>
2017-04-09 01:18:06 -0400

    testing buying

--- start.js
+++ start.js
@@ -1,5 +1,7 @@
 "use strict";
-
+const _ = require('lodash');
+const async = require('async');
+const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
@@ -26,45 +28,163 @@ app.get('/webhook', function(req, res) {
         return res.status(200).send(req.query['hub.challenge']);
     }
     console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
+    return res.sendStatus(403);
 });
 
 app.post('/webhook', function (req, res) {
 
     const data = req.body;
 
-    // Make sure this is a page subscription
-    if (data.object === 'page') {
+    /**
+     * Ignore updates that are not for the page.
+     */
+    if (!(data.object === 'page' && _.isArray(data.entry))) {
 
-        console.log(JSON.stringify(data));
+        return res.sendStatus(200);
+    }
 
-        // Iterate over each entry - there may be multiple if batched
-        // data.entry.forEach(function(entry) {
-        //     const pageID = entry.id;
-        //     const timeOfEvent = entry.time;
-        //
-        //     // Iterate over each messaging event
-        //     entry.messaging.forEach(function(event) {
-        //         if (event.message) {
-        //             receivedMessage(event);
-        //         } else {
-        //             console.log("Webhook received unknown event: ", event);
-        //         }
-        //     });
-        // });
+    async.each(data.entry, (entry, callback) => {
 
-        // Assume all went well.
-        //
+        if (_.isArray(entry.messaging)) {
+
+            return async.each(entry.messaging, handleMessagingEvent, callback);
+        }
+        if (_.isArray(entry.changes)) {
+
+            return async.each(entry.changes, handleChangeEvent, callback);
+        }
+        callback();
+
+    }, (err) => {
+
+        if (err) {
+            return res.sendStatus(500);
+        }
         // You must send back a 200, within 20 seconds, to let us know
         // you've successfully received the callback. Otherwise, the request
         // will time out and we will keep trying to resend.
-        res.sendStatus(200);
-    }
+        return res.sendStatus(200);
+    });
+
+
 });
 
-function receivedMessage(event) {
-    // Putting a stub for now, we'll expand it in the following steps
-    console.log("Message data: ", event.message);
+function handleChangeEvent(changeEvent, callback) {
+
+    if (changeEvent.field !== 'feed' || !_.isPlainObject(changeEvent.value)) {
+        return callback();
+    }
+    if (changeEvent.value.item === 'comment' && changeEvent.value.verb === 'add') {
+
+        tryToBuy(changeEvent.value.sender_id, 'comment');
+
+    } else if (changeEvent.value.item === 'like' && changeEvent.value.verb === 'add') {
+
+        tryToBuy(changeEvent.value.sender_id, 'like');
+    }
+    callback();
+}
+
+function handleMessagingEvent(messagingEvent, callback) {
+
+    console.log('messaging event');
+    console.log(JSON.stringify(messagingEvent));
+    callback();
+}
+
+function tryToBuy(userId, triggerType) {
+
+    console.log('buy triggered by', triggerType);
+
+    const messageData = {
+        "recipient":{
+            "id": userId
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                        {
+                            "title":"#instabuy",
+                            "image_url":"http://www.mysnuggiestore.com/content/images/thumbs/0001326_snuggie-blue.jpeg",
+                            "subtitle":"#instabuy this snuggie",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "http://www.mysnuggiestore.com/snuggie-up-blue-2",
+                                "messenger_extensions": true,
+                                "webview_height_ratio": "tall",
+                                "fallback_url": "http://www.mysnuggiestore.com/snuggies"
+                            },
+                            "buttons":[
+                                {
+                                    "type":"payment",
+                                    "title":"buy",
+                                    "payload":"DEVELOPER_DEFINED_PAYLOAD",
+                                    "payment_summary":{
+                                        "currency":"USD",
+                                        "payment_type":"FIXED_AMOUNT",
+                                        "is_test_payment" : true,
+                                        "merchant_name":"Shaun's Snuggies",
+                                        "requested_user_info":[
+                                            "shipping_address",
+                                            "contact_name",
+                                            "contact_phone",
+                                            "contact_email"
+                                        ],
+                                        "price_list":[
+                                            {
+                                                "label":"Subtotal",
+                                                "amount":"29.99"
+                                            },
+                                            {
+                                                "label":"Taxes",
+                                                "amount":"2.47"
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll 
+ * get the message id in a response 
+ *
+ */
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: PAGE_TOKEN },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const recipientId = body.recipient_id;
+            const messageId = body.message_id;
+
+            if (messageId) {
+                console.log("Successfully sent message with id %s to recipient %s",
+                    messageId, recipientId);
+            } else {
+                console.log("Successfully called Send API for recipient %s",
+                    recipientId);
+            }
+        } else {
+            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+        }
+    });
 }
 
 /*
