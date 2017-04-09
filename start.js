596067a5c726218ce567f5f964c06e9a1657a3c6
shaunpersad <shaunpersad@gmail.com>
2017-04-08 21:09:29 -0400

    testing basic functions

--- start.js
+++ start.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+
 const app = express();
+
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
 
 const PORT = process.env.PORT || 3000;
 const VERIFY_TOKEN = 'EmbnhP2tMuG1UQDscaX0GrXXzbebec3PVoxJljXWfIaFU5FNI1';
 const PAGE_TOKEN = 'EAADcZBBJyxP0BAKwrxisgMvewalZBNc2atXnE9cKNziPYQObyTPfRy56U6TXD0ZCiSGKmMkj2c9drQlvlC7biu1ZCzoVoXbqZBfmPO9WXBR9ZC10Qy9b29jaB56eTOfAoW6knhfMJZCRXUtlxI6mENLSSZCZB2zajhoO4mMZCLAQ4NIgZDZD';
+const APP_SECRET = 'f1fe035a41452301b87acaf667d14478';
 
 app.get('/', function(req, res) {
 
@@ -23,7 +30,7 @@ app.get('/webhook', function(req, res) {
 });
 
 app.post('/webhook', function (req, res) {
-    
+
     const data = req.body;
 
     // Make sure this is a page subscription
@@ -56,6 +63,37 @@ app.post('/webhook', function (req, res) {
 function receivedMessage(event) {
     // Putting a stub for now, we'll expand it in the following steps
     console.log("Message data: ", event.message);
+}
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from 
+ * the App Dashboard, we can verify the signature that is sent with each 
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+
+    const signature = req.headers['x-hub-signature'];
+
+    if (!signature) {
+        // For testing, let's log an error. In production, you should throw an 
+        // error.
+        console.error("Couldn't validate the signature.");
+    } else {
+        const elements = signature.split('=');
+        const method = elements[0];
+        const signatureHash = elements[1];
+
+        const expectedHash = crypto.createHmac('sha1', APP_SECRET)
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash !== expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
 }
 
 
